4b96ad86886cddd61f247ce5e4ee8a7382c0ea96
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-02-18 15:48:29 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -22,35 +22,40 @@ void print_fibonacci_first_92(unsigned long fib1, unsigned long fib2)
 }
 
 /**
- * print_fibonacci_large - Prints Fibonacci numbers above 92th term
- * @fib1_half1: First half of fib1
- * @fib1_half2: Second half of fib1
- * @fib2_half1: First half of fib2
- * @fib2_half2: Second half of fib2
+ * print_fibonacci_large - Prints Fibonacci numbers beyond the 92nd term
+ * @f1_h1: First half of fib1
+ * @f1_h2: Second half of fib1
+ * @f2_h1: First half of fib2
+ * @f2_h2: Second half of fib2
  */
-void print_fibonacci_large(unsigned long fib1_half1, unsigned long fib1_half2,
-						   unsigned long fib2_half1, unsigned long fib2_half2)
+void print_fibonacci_large(unsigned long f1_h1, unsigned long f1_h2,
+			   unsigned long f2_h1, unsigned long f2_h2)
 {
 	int count;
-	unsigned long half1, half2;
+	unsigned long h1, h2;
 
 	for (count = 93; count <= 98; count++)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
+		h1 = f1_h1 + f2_h1;
+		h2 = f1_h2 + f2_h2;
 
-		if (half2 >= 1000000000)
+		/* Handle overflow by carrying to the higher half */
+		if (h2 >= 1000000000)
 		{
-			half1 += 1;
-			half2 %= 1000000000;
+			h1 += 1;
+			h2 %= 1000000000;
 		}
 
-		printf(", %lu%09lu", half1, half2);
+		/* Avoid leading zeros in large Fibonacci numbers */
+		if (h1 == 0)
+			printf(", %lu", h2);
+		else
+			printf(", %lu%09lu", h1, h2);
 
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		f1_h1 = f2_h1;
+		f1_h2 = f2_h2;
+		f2_h1 = h1;
+		f2_h2 = h2;
 	}
 }
 
@@ -62,16 +67,17 @@ void print_fibonacci_large(unsigned long fib1_half1, unsigned long fib1_half2,
 int main(void)
 {
 	unsigned long fib1 = 1, fib2 = 2;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
 
 	print_fibonacci_first_92(fib1, fib2);
 
-	fib1_half1 = fib1 / 1000000000;
-	fib1_half2 = fib1 % 1000000000;
-	fib2_half1 = fib2 / 1000000000;
-	fib2_half2 = fib2 % 1000000000;
+	/* Split numbers into two halves to handle overflow */
+	f1_h1 = fib1 / 1000000000;
+	f1_h2 = fib1 % 1000000000;
+	f2_h1 = fib2 / 1000000000;
+	f2_h2 = fib2 % 1000000000;
 
-	print_fibonacci_large(fib1_half1, fib1_half2, fib2_half1, fib2_half2);
+	print_fibonacci_large(f1_h1, f1_h2, f2_h1, f2_h2);
 
 	printf("\n");
 	return (0);
