0a34ab121159139b51f3302b263973f550af6f7e
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-02-25 14:33:36 +0000

    _

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,17 +10,19 @@ int _atoi(char *s)
 {
 	int i = 0, sign = 1, result = 0;
 
-	/* Skip non-numeric characters */
-	while (s[i] != '\0')
+	/* Skip leading whitespace characters */
+	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))
+		i++;
+
+	/* Handle signs ('+' or '-') */
+	while (s[i] != '\0' && (s[i] == '+' || s[i] == '-'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			break;
 		i++;
 	}
 
-	/* Convert string to integer */
+	/* Convert the string to an integer */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');
