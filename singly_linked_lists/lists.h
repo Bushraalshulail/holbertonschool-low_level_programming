8eb0343e98fedd7fb432f654edb36cc8ffdac3b8
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-04-02 03:11:28 +0000

    _

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -8,14 +8,17 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: Node structure for a singly linked list
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
+
